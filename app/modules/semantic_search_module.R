5ae036957a182f832e1bc5feac279d36fd5046e8
jamiehshare <jamie.hudson@sharecreative.com>
2024-05-22 12:29:45 +0100

    Updated semantic search

--- app/modules/semantic_search_module.R
+++ app/modules/semantic_search_module.R
@@ -3,7 +3,7 @@ semantic_search_UI <- function(id) {
   shiny::tagList(
     # sidebarPanel(
     shiny::textInput(NS(id, "search_term"), "Enter search term:", value = "AI art"),
-    shiny::numericInput(NS(id, "cosine_sim_thresholds"), "Cosine similarity threshold", value = 0.3, min = 0, max = 1, step = 0.1),
+    shiny::numericInput(NS(id, "dot_prod_threshold"), "Dot product threshold", value = 5, min = 0, max = 10, step = 0.2),
     shiny::actionButton(NS(id, "update_plot"), "Update Plot")
     # )
   )
@@ -15,10 +15,12 @@ semantic_searchServer <- function(id, r) {
     ns <- session$ns
     
     # source("~/Documents/git-repos/demoApp/demoApp/R/semantic_search_helper_functions.R")
+
     example_sentences <- readr::read_csv("~/Google Drive/My Drive/Share_Clients/data_science_project_work/hackafun/data/semantic_search_data/displayr_example_sentences.csv") %>%
       dplyr::rename(rowid = row_id)
-
-    multi_qa_matrix_sentences <- readr::read_rds("~/Google Drive/My Drive/Share_Clients/data_science_project_work/hackafun/data/semantic_search_data/example_multi-qa_embeddings_sentences.rds")
+    
+    multi_qa_matrix_sentences <- readr::read_csv(here::here("app/data/cosmetic_sentences_embeddings_quant.csv")) %>% 
+                                                   as.matrix()
     
     # Reactive values to store intermediate data
     reactive_vals <- reactiveValues(
@@ -32,9 +34,9 @@ semantic_searchServer <- function(id, r) {
     
     observeEvent(input$update_plot, {
       print("searching")
-      semantic_similarity_output <- cosine_calculation_threshold_sentence(
+      semantic_similarity_output <- quant_dot_product_threshold_sentence(
         reference_statement = input$search_term,
-        cosine_sim_threshold = input$cosine_sim_thresholds,
+        cosine_sim_threshold = input$dot_prod_threshold,
         embedding_model = "multi-qa-mpnet-base-cos-v1",
         sentence_matrix = multi_qa_matrix_sentences,
         df = example_sentences
